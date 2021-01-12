3a2ce8f21848ef72b1c109b515f5e0ae8bd697bc
stryku <stryku2393@gmail.com>
2021-01-12 19:45:45 +0100

    Removed unneeded stuff

--- bin/run-dziadzia.sh
+++ bin/run-dziadzia.sh
@@ -5,17 +5,6 @@ if [ ! -f DeepSpeech.py ]; then
     exit 1
 fi;
 
-if [ ! -f "data/ldc93s1/ldc93s1.csv" ]; then
-    echo "Downloading and preprocessing LDC93S1 example data, saving in ./data/ldc93s1."
-    python -u bin/import_ldc93s1.py ./data/ldc93s1
-fi;
-
-if [ -d "${COMPUTE_KEEP_DIR}" ]; then
-    checkpoint_dir=$COMPUTE_KEEP_DIR
-else
-    checkpoint_dir=$(python -c 'from xdg import BaseDirectory as xdg; print(xdg.save_data_path("deepspeech/ldc93s1"))')
-fi
-
 # Force only one visible device because we have a single-sample dataset
 # and when trying to run on multiple devices (like GPUs), this will break
 export CUDA_VISIBLE_DEVICES=0
